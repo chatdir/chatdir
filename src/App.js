a075d0bd71ee5fdc91130762a17b9b6db63b50c3
Aidan Bane <aidanfbane@gmail.com>
2018-10-13 08:21:19 -0700

    config(webpack.config.js): Add webpack-dev-server and start script

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 import './App.css'
+import PhoneText from './img/phoneText.jpg'
 
 class App extends Component {
   state = {}
   render() {
-    return <div className="AppClass">Hello, world!</div>
+    return (
+      <div className="AppClass">
+        Hello, world!
+        <div>
+          <img src={PhoneText} alt="example chat on a phone" />
+        </div>
+      </div>
+    )
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
   module: {
     rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
@@ -20,5 +22,22 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
     ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      filename: 'dist/index.html',
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    port: 3000,
+    publicPath: 'http://localhost:3000/dist/',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
   },
 }
