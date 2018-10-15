bf187b854f500505b44e68e23a37a57d07cf0af0
Aidan Bane <aidanfbane@gmail.com>
2018-10-15 01:41:07 -0700

    feat(webpackConfig): Update webpack config
    -split webpack config and moved to build-utils folder

--- __tests__/App.spec.js
+++ __tests__/App.spec.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { create } from "react-test-renderer";
-import App from "../src/App";
+import React from 'react'
+import { create } from 'react-test-renderer'
+import App from '../src/App'
 
-describe("App", () => {
-  test("snapshot", () => {
-    const input = create(<App />);
-    expect(input.toJSON()).toMatchSnapshot();
-  });
-});
+describe('App', () => {
+  test('snapshot', () => {
+    const input = create(<App />)
+    expect(input.toJSON()).toMatchSnapshot()
+  })
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,80 @@
 const path = require('path')
+// const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+// const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpackMerge = require('webpack-merge')
+const loadPresets = require('./build-utils/loadPresets')
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env)
 
-module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader'],
+module.exports = ({ mode, presets } = { mode: 'production', presets: [] }) =>
+  webpackMerge(
+    {
+      entry: {
+        app: './src/index.js',
       },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        options: { presets: ['@babel/env'] },
+      module: {
+        rules: [
+          {
+            test: /\.(png|svg|jpe?g|gif)$/,
+            use: [
+              {
+                loader: 'url-loader',
+                options: {
+                  limit: 5000,
+                },
+              },
+            ],
+          },
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader',
+            options: { presets: ['@babel/env'] },
+          },
+        ],
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+      plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+      output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
       },
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      filename: 'dist/index.html',
-      template: './public/index.html',
-    }),
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, 'public/'),
-    port: 3000,
-    publicPath: 'http://localhost:3000/dist/',
-    hot: true,
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist/'),
-    publicPath: '/dist/',
-    filename: 'bundle.js',
-  },
-}
+    },
+    modeConfig(mode),
+    loadPresets({ mode, presets })
+  )
+
+// const common = {
+//   entry: {
+//     app: './src/index.js',
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(png|svg|jpg|gif)$/,
+//         use: ['file-loader'],
+//       },
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /(node_modules|bower_components)/,
+//         loader: 'babel-loader',
+//         options: { presets: ['@babel/env'] },
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new CleanWebpackPlugin(['dist']),
+//     new HtmlWebpackPlugin({
+//       template: './public/index.html',
+//     }),
+//   ],
+//   output: {
+//     filename: '[name].bundle.js',
+//     path: path.resolve(__dirname, 'dist'),
+//   },
+// }
+
+// module.exports = config => merge(common, config)
