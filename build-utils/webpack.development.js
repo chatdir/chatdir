34b2876cd6c3ae27fc144751520187ff7efb456c
Aidan Bane <aidanfbane@gmail.com>
2018-10-28 02:20:24 -0700

    (config): Add config for css-modules (#5)

--- build-utils/webpack.development.js
+++ build-utils/webpack.development.js
@@ -1,10 +1,21 @@
 module.exports = () => ({
   mode: 'development',
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: true,
+              localIdentName: '[name]__[local]--[hash:base64:5]',
+            },
+          },
+        ],
       },
     ],
   },
--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -1,16 +1,43 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = () => ({
   output: {
     filename: '[chunkhash].bundle.js',
   },
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+      }),
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: true,
+              localIdentName: '[hash:base64:5]',
+            },
+          },
+        ],
       },
     ],
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
 })
